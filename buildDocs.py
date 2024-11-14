eedb7fcef162a73c77f0fb29280c9c135fdbc0f6
SURAKSHITH99453 <57933760+SURAKSHITH99453@users.noreply.github.com>
2024-11-14 15:22:50 +0530

    Update buildDocs.py

--- buildDocs.py
+++ buildDocs.py
@@ -12,8 +12,8 @@ for i, file in enumerate(files):
     print(f"{i + 1}. {file}")
 
 # Get user input
-selections = input("Enter the numbers of the files you want to include (separated by spaces): ")
-
+#selections = input("Enter the numbers of the files you want to include (separated by spaces): ")
+selections = "1 2 3 4 5 6 7"
 # Convert selections to filenames
 selected_files = []
 for num in selections.split():
@@ -33,14 +33,14 @@ print("Selected files:", os.environ["SELECTED_FILES"])
 
 # Clean previous build if exists
 build_dir = "build"
-if os.path.exists(build_dir):
-    shutil.rmtree(build_dir)
+#if os.path.exists(build_dir):
+#    shutil.rmtree(build_dir)
 
 # Run Antora
-subprocess.run(["C:/Program Files/nodejs/npx.cmd", "antora", "antora-playbook.yml"])
+subprocess.run(["npx", "antora", "antora-playbook.yml"])
 
 # Specify the desired output directory
-custom_output_dir = "C:/Nexeed/Docs-output"
+custom_output_dir = "/home/demo/output"
 os.makedirs(custom_output_dir, exist_ok=True)
 
 # Clean previous output if exists
@@ -48,5 +48,7 @@ if os.path.exists(custom_output_dir):
     shutil.rmtree(custom_output_dir)
 
 # Move the build output to the custom directory
-shutil.move(build_dir, custom_output_dir)
+shutil.copytree(build_dir, custom_output_dir, dirs_exist_ok=True)
+shutil.rmtree(build_dir)
+
 print(f"Build output moved to {custom_output_dir}")
